f47888edf826b9edd08a58edca7ec361267d1046
Tara Foulkrod <photochik02@hotmail.com>
2017-09-19 15:52:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+//Notes and Objectives
+//kittens = ['Milo', 'Otis', 'Garfield']
+//appending and removing 'Ralph'
+//destructive vs non-destructive
+//make sure i know these well
+//make sad dead kitty happy and alive by passing tests
+
+function destrucivelyAppendKitten() {
+  kittens.unshift()
+  return kittens
+}
